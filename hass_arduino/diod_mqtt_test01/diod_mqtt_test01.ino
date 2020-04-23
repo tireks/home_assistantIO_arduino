c11402a1b844bd7f53d91eac431f7fd88d240558
Ruslan Leonov <leonru007@gmail.com>
2020-04-23 14:49:39 +0700

    fixed controlling by mqtt switcher

--- hass_arduino/diod_mqtt_test01/diod_mqtt_test01.ino
+++ hass_arduino/diod_mqtt_test01/diod_mqtt_test01.ino
@@ -8,7 +8,7 @@
 
 const int buttonPin = 40; // номер порта нашей кнопки
 const int ledPin =    48; // номер порта светодиода
-int button_output_flag = 0; //flag for serial output of button state
+int button_output_flag = 1; //flag for serial output of button state
 int button_activ_flag[2] = {-1,0};
 #define DEBUG 1 // Debug output to serial console
 #define mqtt_user "mqttusr"
@@ -22,7 +22,7 @@ char* switchTopic = "home-assistant/controller_switches/sensor_diod";
 int sensorPin = 48; // Pin to which the sensor is connected to
 char buf[4]; // Buffer to store the sensor value
 int updateInterval = 1000; // Interval in milliseconds
-int subSignal = 0;
+int subSignal = -1;
 
 // MQTT server settings
 byte mqttServer[] = {192, 168, 88, 17};
@@ -63,11 +63,11 @@ void callback(char* topic, byte* payload, unsigned int length)
   Serial.println(strPayload);
   if (strTopic == "home-assistant/controller_switches/sensor_diod")
   {
-    if (strPayload == "ON_")
+    if (strPayload == "on_")
     {
       subSignal = 1;
     }
-    if (strPayload == "OFF")
+    if (strPayload == "off")
     {
       subSignal = -1;
     }
@@ -91,7 +91,7 @@ void loop() {
     // читаем состояние порта кнопки и записываем в переменную
     int buttonState = digitalRead(buttonPin);
     // делаем простую проверку нашей переменной, если на входе в порт кнопки присутствует напряжение - включаем светодиод, иначе - выключаем
-    if (buttonState == LOW) 
+    if (buttonState == LOW || subSignal != 0) 
     {
       if ((button_activ_flag[1] == 0 && button_activ_flag[0] == -1) || subSignal == 1)
       {
@@ -117,21 +117,22 @@ void loop() {
         button_activ_flag[1] = -1;
       }
     }*/
-    if (buttonState == HIGH && button_activ_flag[1] != 0)
+    if ((buttonState == HIGH && button_activ_flag[1] != 0) || subSignal != 0)
     {
       button_activ_flag[1] = 0;
+      subSignal = 0;
     }
-      if (button_activ_flag[0] == 1 && button_activ_flag[1] == 0)
+    if (button_activ_flag[0] == 1 && button_activ_flag[1] == 0)
+    {
+      digitalWrite(ledPin, HIGH);
+      if (button_output_flag == 0) 
       {
-        digitalWrite(ledPin, HIGH);
-        if (button_output_flag == 0) 
-        {
-          Serial.println("button_on");
-          strcpy(buf, "on_");
-          client.publish(stateTopic, buf);
-          button_output_flag = 1;
-        }
+        Serial.println("button_on");
+        strcpy(buf, "on_");
+        client.publish(stateTopic, buf);
+        button_output_flag = 1;
       }
+    }
     
      if (button_activ_flag[0] == -1 && button_activ_flag[1] == 0) {
         // выключаем светодиод
@@ -146,4 +147,5 @@ void loop() {
         
     }
 }
+
 
